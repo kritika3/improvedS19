81e6bc200a32f4c259182be419b755ea68c8b4dd
Ubuntu <ubuntu@ip-172-31-10-107.ap-south-1.compute.internal>
2019-06-12 10:31:10 +0000

    Problem3

---
+++ prob3.py
@@ -0,0 +1,14 @@
+#!/usr/bin
+adhoc=[1,2,3,1,4,5,66,22,2,6,0,9]
+g5=list()
+le2=list()
+print('Numbers greater than 5')
+for i in adhoc:
+    if(i>5):
+        g5=i
+        print(g5)
+print('Numbers less than or equal to 2')
+for i in adhoc:
+    if(i<=2):
+        le2=i
+        print(le2)
